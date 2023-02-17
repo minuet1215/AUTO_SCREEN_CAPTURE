f814585fc2123e8be3ab34f2fe14bbf58bd68c75
minu <brotheratoz3@naver.com>
2023-02-18 01:48:53 +0900

    style: remove comments

--- image_to_pdf.py
+++ image_to_pdf.py
@@ -7,20 +7,15 @@ from natsort import natsorted
 image_files = natsorted([file for file in os.listdir('.') if file.endswith(
     '.jpg') or file.endswith('.png') or file.endswith('.gif')])
 
-# Create a new PDF file
 pdf_merger = PdfFileMerger()
 
-# Iterate through the image files and add them to the PDF merger
 for file in image_files:
-    # Open the image and convert it to PDF format
     image = Image.open(file)
     pdf_image = image.convert('RGB')
 
-    # Add the converted image to the PDF file
     pdf_page = BytesIO()
     pdf_image.save(pdf_page, format='PDF', quality=95)
     pdf_merger.append(PdfFileReader(pdf_page))
 
-# Save the PDF file
 with open('output.pdf', 'wb') as f:
     pdf_merger.write(f)
