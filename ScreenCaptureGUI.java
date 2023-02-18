f17fbfeacbc02dcd4d1148c881f4087127236988
minu <brotheratoz3@naver.com>
2023-02-18 17:54:36 +0900

    feat: improve captured image quailty

--- ScreenCaptureGUI.java
+++ ScreenCaptureGUI.java
@@ -1,17 +1,20 @@
-
 import com.github.kwhat.jnativehook.GlobalScreen;
 import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
-
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.FileImageOutputStream;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Iterator;
 
 public class ScreenCaptureGUI implements NativeKeyListener {
     private JFrame frame;
@@ -50,14 +53,22 @@ public class ScreenCaptureGUI implements NativeKeyListener {
     private void captureScreen() {
         try {
             Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-
             BufferedImage capture = new Robot().createScreenCapture(screenRect);
 
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
-            String fileName = "screenshot_" + dateFormat.format(new Date()) + ".png";
+            String fileName = "screenshot_" + dateFormat.format(new Date()) + ".jpeg";
 
             File output = new File(fileName);
-            ImageIO.write(capture, "png", output);
+
+            Iterator<ImageWriter> iter = ImageIO.getImageWritersByFormatName("jpeg");
+            ImageWriter writer = iter.next();
+            ImageWriteParam iwp = writer.getDefaultWriteParam();
+            iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+            iwp.setCompressionQuality(1.0f);
+
+            FileImageOutputStream outputImage = new FileImageOutputStream(output);
+            writer.setOutput(outputImage);
+            writer.write(null, new IIOImage(capture, null, null), iwp);
 
             ImageIcon icon = new ImageIcon(capture);
             imageLabel.setIcon(icon);
